d157ff2595049b9e0f24d95875252ea10798e075
Benck233 <169964@upf.br>
2026-03-15 00:58:49 -0300

    atividade_.py

---
+++ code_exercicio_2.py
@@ -0,0 +1,7 @@
+nota_aluno = 7
+
+if nota_aluno >=6:
+    print("Aprovado")
+
+else:
+    print("Reprovado")
---
+++ code_exercicio_3.py
@@ -0,0 +1,19 @@
+nota = 9
+
+
+if nota >=9.0:
+    conceito = "A"
+
+elif nota >=8.0:
+    conceito = "B" 
+
+elif nota >= 7.0:
+    conceito = "C"
+
+elif nota >= 6.0:
+    conceito = "D"
+
+else:
+    conceito = "F"
+
+print(f"Seu conceito é {conceito}")
