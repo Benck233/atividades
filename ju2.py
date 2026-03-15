aff676a7a1dd76e2fe37d94ce4a0e3dc18a8945a
Benck233 <169964@upf.br>
2026-03-15 00:13:13 -0300

    exer_code_4
    Co-authored-by: Rafael-Dpaula <Rafael-Dpaula@users.noreply.github.com>

---
+++ ju2.py
@@ -0,0 +1,66 @@
+print("helloWolrd")
+
+
+
+
+#soma zone
+
+
+a=5 
+b =10
+c = a+b
+
+print(3/1.7)
+print(3*7772)
+
+print(a + b)
+
+print(a + b -6)
+
+resultado = a+b
+
+
+
+print("resultado pelo c = a+b", c)
+print(f"o resultado da sua conta é {resultado}")
+
+print("o resultado da sua conta é", resultado )
+#variaves sao caixnhas de valor
+#string é um conjunto de caracteris
+#int = numeros inteiros
+#float = numeros com casas decimais
+#boolean = verdadeiro ou falso
+
+idade = 20
+tem_documento = True
+
+pode_entrar = idade >= 18 and tem_documento
+print(pode_entrar)  # True
+
+fim_de_semana = False
+tem_feriado = True
+descanso = fim_de_semana or tem_feriado
+print(descanso)     # True
+
+print(not descanso) # False
+
+#and
+#v v = v 
+#v f = f
+#f v = f
+#f f = f
+#or
+#v v = v
+#v f = v
+#f v = v
+#f f = f
+
+nome_aluno = "bobber"
+
+nota_1 = 8.0
+nota_2 = 7.5
+nota_3 = 9.0
+media = (nota_1 +nota_2 +nota_3)/3
+
+print(f"Nota de {nome_aluno} é ")
+print(media)
