113dc896161cc2936f51b91147ea8e030f7abfd1
Benck233 <169964@upf.br>
2026-03-17 19:55:02 -0300

    coment_pass

--- code_def_1.py
+++ code_def_1.py
@@ -2,3 +2,7 @@ def dobro_valor(valor): # os 2 pontos servem para definir aonde acaba a definiç
     print(valor*2)
 
 dobro_valor(7)
+
+
+def calcula ():
+    pass   # o pass server para deixar o bloco sem nada e fazer nada
