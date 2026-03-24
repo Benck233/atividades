ac96405d30f46531ade2adb762fd6f2ff1351c1f
Benck <169964@upf.br>
2026-03-24 20:20:51 -0300

    wdklw

--- biblioteca_pi.py
+++ biblioteca_pi.py
@@ -1,3 +1,21 @@
-import math
+import math 
 
 print(math.pi)
+
+import math
+
+print(type(math.pi))           # 3.141592653589793 type(serve para informar que tipo de classe é)
+print(math.sqrt(16))     # 4.0
+print(math.ceil(2.3))    # 3  (arredonda para cima)
+print(math.floor(2.9))   # 2  (arredonda para baixo)
+print(math.pow(2, 10))   # 1024.0
+
+
+
+import random
+
+print(random.randint(1, 6))    # número inteiro entre 1 e 6
+print(random.random())         # número float entre 0.0 e 1.0
+
+nomes = ["Alice", "Bob", "Carol"]
+print(random.choice(nomes))    # escolhe um elemento da lista
