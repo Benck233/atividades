664f58f4a586cf9da171e6276418ba047ac2d096
GUILHERME BENCK DE MELLO <398456@mundiart.com.br>
2026-05-08 16:23:34 -0300

    estufos

---
+++ codigos_py/exercicios_estudo/estudo_11.py
@@ -0,0 +1,11 @@
+n1=int(input("Digite um número inteiro: "))
+n2=int(input("Digite um número inteiro: "))
+real=float(input("Digite um numero real: "))
+
+dobro_produto=(2*n1)*(n2/2)
+triplo_produto=(3*n1)+real
+elevado_produto=(real**3)
+
+print(dobro_produto)
+print(triplo_produto)
+print(elevado_produto)
---
+++ codigos_py/exercicios_estudo/estudo_12.py
@@ -0,0 +1,5 @@
+tamanho_gigabytes=float(input("Digite o tamanho do arquivo em Gigabytes: "))
+
+megabytes=tamanho_gigabytes*1024
+
+print(f"O tamanho do arquivo em megabytes é de: {megabytes}")
