dba04bec860972417974ebef2f584fc978cb48b4
GUILHERME BENCK DE MELLO <398456@mundiart.com.br>
2026-05-08 15:37:05 -0300

    estudos

--- codigos_py/exercicios_estudo/estuo_8.py
+++ codigos_py/exercicios_estudo/estuo_8.py
@@ -1,1 +1,6 @@
-float(input("Quanto você ganh por hora: "))
+valor=float(input("Quanto você ganha por hora: "))
+hora=float(input("Quantas horas você trablha por mês: "))
+
+salario=hora*valor
+
+print(f"Seu salario no mês é de {salario}")
