f2e0d912b73cd1d8dbb2e83d255246b599ddd7c5
GUILHERME BENCK DE MELLO <398456@mundiart.com.br>
2026-05-08 16:09:08 -0300

    estudos

---
+++ codigos_py/exercicios_estudo/estudo_10.py
@@ -0,0 +1,5 @@
+tempc=float(input("Digite a temperatura em Celsius: "))
+
+far=(tempc * 9/5) + 32
+
+print(f"A temperaura em fahrenheit é: {far}")
---
+++ codigos_py/exercicios_estudo/estudo_9.py
@@ -0,0 +1,5 @@
+far=float(input("Digite a temperatura em Fahrenheit: "))
+
+tempc= 5 * ((far-32) / 9)
+
+print(f"A temperatura em Graus é: {tempc}")
