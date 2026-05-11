4977fb865224d67d7c20088b779db03292b04862
GUILHERME BENCK DE MELLO <398456@mundiart.com.br>
2026-05-11 15:17:18 -0300

    estudos

---
+++ codigos_py/exercicios_estudo/estudo_13.py
@@ -0,0 +1,7 @@
+gigabytes=float(input("Digite o tamanho do arquivo em Gigabytes: "))
+
+megabytes= gigabytes *1024
+kylobytes= gigabytes *1024*1024
+
+print(f"O tamanho do seu arquivo em megabytes é: {megabytes}")
+print(f"O tamanho do seu aqruivo em kilobytes é: {kylobytes}")
---
+++ codigos_py/exercicios_estudo/estuo_14.py
@@ -0,0 +1,11 @@
+peixe=float(input("Digite o peso do peixe: "))
+
+
+def peixe_multa(peixe):
+    multa=0
+    while peixe>50:
+        multa=+4
+    print(multa)
+
+
+peixe_multa(peixe)
