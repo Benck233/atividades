ca39c543803776723734274ff4b3f3aaf9023b72
Benck233 <169964@upf.br>
2026-05-15 17:01:44 -0300

    estudo

---
+++ pasta_estudo/estudo_com_IA/estudo_16.py
@@ -0,0 +1,6 @@
+cateto_opo=float(input("Digite o cateto oposto: "))
+cateto_adj=float(input("Digite o cateto adjacente: "))
+
+hipo=(cateto_opo**2)+(cateto_adj**2)
+
+print(f"A hipotenusa é: {hipo}")
---
+++ pasta_estudo/estudo_com_IA/estudo_17.py
@@ -0,0 +1,7 @@
+angulo=float(input("Digite um angulo: "))
+
+seno=[1/2,2**2(1/2)/2,3**2(2/1)/2]
+
+for an in seno:
+    if angulo == seno:
+        print(seno)
