b0db4af1256928b7a01c4d5592f36ad0d077dda8
Benck233 <169964@upf.br>
2026-05-20 13:37:06 -0300

    estudo_loops

--- pasta_estudo/estudo_com_IA/estudo_loops/exercicio_loops.py
+++ pasta_estudo/estudo_com_IA/estudo_loops/exercicio_loops.py
@@ -25,3 +25,26 @@ produtos=["Arroz", "Feijão", "Sabão", "Massa", "Azeite"]
 for tamanho in produtos:
     if len(tamanho) == 5:
         print(f"{tamanho}")
+
+
+# Se usássemos um dicionário para o resultado:
+resultado = {"Notebook": 3500}
+
+print(resultado)
+
+
+precos=[25.0, 80.0, 12.0, 45.0, 60.0, 8.0]
+soma=0
+
+for maior in precos:
+    soma+=maior
+
+media=soma/5
+media_final=0
+
+for mediana in precos:
+    if mediana > media:
+        media_final+=1
+
+
+print(media_final)
