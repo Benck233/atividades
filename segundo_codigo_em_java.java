dc0c3cffdb8af20c6157ff8ad9c97b319e7a9239
Benck233 <169964@upf.br>
2026-06-11 10:33:05 -0300

    ippiffo

---
+++ segundo_codigo_em_java.java
@@ -0,0 +1,9 @@
+public class segundo_codigo_em_java {
+
+    public static void main(String[]args){
+        String cliente= "Guilherme Benck";
+
+        System.out.println("ola seja muito bem vindo "+cliente);
+
+    }
+}
