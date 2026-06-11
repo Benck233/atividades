fc1ba539b60f37c0fc46c0e316b9b2a4798cc4ae
Benck233 <169964@upf.br>
2026-06-11 12:03:54 -0300

    java_dos_ippiffos

--- segundo_codigo_em_java.java
+++ segundo_codigo_em_java.java
@@ -10,6 +10,7 @@ public class segundo_codigo_em_java {
         System.out.println("ola seja muito bem vindo "+cliente);
 
         System.out.println("ola: "+cliente +" você comprou 10 itens, o total a pagar é: "+ preco_produto*quantidade+" reais" );
-
+        //formatação//
+       // System.out.printf("%.2f",preco_produto*quantidade);
     }
 }
