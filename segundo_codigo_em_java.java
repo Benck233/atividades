5d1caede2ce08282639c885f6b12908b169e5fde
Benck233 <169964@upf.br>
2026-06-11 10:38:25 -0300

    ippiffinho_perigoso

--- segundo_codigo_em_java.java
+++ segundo_codigo_em_java.java
@@ -1,9 +1,15 @@
 public class segundo_codigo_em_java {
 
     public static void main(String[]args){
-        String cliente= "Guilherme Benck";
+        int quantidade = 10;
+        int preco_produto = 20;
+        System.out.println(quantidade * preco_produto);
 
+
+        String cliente= "Guilherme Benck";
         System.out.println("ola seja muito bem vindo "+cliente);
+
+        System.out.println("ola: "+cliente +" você comprou 10 itens, o total a pagar é: "+ preco_produto*quantidade+" reais" );
 
     }
 }
