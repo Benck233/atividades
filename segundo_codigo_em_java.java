39df18876af719d867fcf2d57eb795f61a121672
Benck233 <169964@upf.br>
2026-06-12 13:44:18 -0300

    linus_turvald

--- segundo_codigo_em_java.java
+++ segundo_codigo_em_java.java
@@ -1,16 +1,27 @@
+import java.util.Scanner;
+
 public class segundo_codigo_em_java {
-
+    
     public static void main(String[]args){
-        int quantidade = 10;
-        int preco_produto = 20;
-        System.out.println(quantidade * preco_produto);
+        Scanner teclado = new Scanner(System.in);
 
 
-        String cliente= "Guilherme Benck";
-        System.out.println("ola seja muito bem vindo "+cliente);
+        System.out.println("Digite seu nome");
+        String cliente= teclado.next();
+    
+        System.out.println("Insira a quantidade");
+        int quantidade = teclado.nextInt();
+
+        double preco_produto = 37.8;
+        //double preco_produto = teclado.nextDouble();//tem que ser usado "," nao ponto "."
+        //System.out.println(quantidade * preco_produto);
+
+
+       // System.out.println("ola seja muito bem vindo "+cliente);
 
         System.out.println("ola: "+cliente +" você comprou 10 itens, o total a pagar é: "+ preco_produto*quantidade+" reais" );
         //formatação//
-       // System.out.printf("%.2f",preco_produto*quantidade);
+        System.out.printf("%.2f",preco_produto*quantidade);
+       teclado.close();
     }
 }
