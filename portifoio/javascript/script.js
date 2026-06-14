9eba0740ce52ec97186f24c80a2ab5e6dbc18205
Benck233 <169964@upf.br>
2026-06-14 20:10:18 -0300

    portifolio

--- portifoio/javascript/script.js
+++ portifoio/javascript/script.js
@@ -1,12 +1,8 @@
-
 const dtToggleBtn = document.getElementById('dark-mode-toggle');
-
 
 if (dtToggleBtn) {
     dtToggleBtn.addEventListener('click', () => {
-        
         document.body.classList.toggle('dark-mode');
-        
         
         if (document.body.classList.contains('dark-mode')) {
             dtToggleBtn.textContent = "☀️ Modo Claro";
@@ -16,15 +12,16 @@ if (dtToggleBtn) {
     });
 }
 
-
-let currentFontSize = 100; 
+let currentFontSize = 100;
 const btnAumentar = document.getElementById('btn-aumentar');
 const btnDiminuir = document.getElementById('btn-diminuir');
 
 if (btnAumentar && btnDiminuir) {
     btnAumentar.addEventListener('click', () => {
-        currentFontSize += 10; 
-        document.body.style.fontSize = currentFontSize + '%';
+        if (currentFontSize < 150) { 
+            currentFontSize += 10; 
+            document.body.style.fontSize = currentFontSize + '%';
+        }
     });
 
     btnDiminuir.addEventListener('click', () => {
@@ -35,16 +32,13 @@ if (btnAumentar && btnDiminuir) {
     });
 }
 
-
 const formContato = document.getElementById('form-contato');
 
 if (formContato) {
     formContato.addEventListener('submit', function(event) {
-        
         const nome = document.getElementById('nome').value.trim();
         const email = document.getElementById('email').value.trim();
         const mensagem = document.getElementById('mensagem').value.trim();
-        
         
         if (nome === "" || email === "" || mensagem === "") {
             alert("Por favor, preencha todos os campos obrigatórios.");
