65ef8985e9cc4c614c0b5d3e2e0cd66ee64ef02d
Benck233 <169964@upf.br>
2026-07-08 17:23:12 -0300

    estudo_php

---
+++ estudos_php/variaveis.php
@@ -0,0 +1,8 @@
+<?php
+$nome = "Alexandre"; // String
+$idade = 25; // Int
+$altura = 1.75; //float
+
+print "Nome: ". $nome . "<br>"; // O ponto (.) serve para juntar (concatenar) textos [15]
+echo "Idade: $idade anos<br>"; // Dentro de aspas duplas, o PHP reconhece a variável diretamente [24]
+?>
