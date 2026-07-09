d3267c608f0f9973f54619737651645b9665684a
Benck233 <169964@upf.br>
2026-07-09 09:48:05 -0300

    estudo_php

---
+++ estudos_php/pagina.php
@@ -0,0 +1,8 @@
+<?php
+
+// Importa as ferramentas do arquivo utilitarios.php
+
+require("utilitarios.php");
+
+echo saudar("Alexandre, "."O grande");// Executa a função que está no outro arquivo
+?>
---
+++ estudos_php/utilitarios.php
@@ -0,0 +1,6 @@
+<?php
+function saudar($nome){
+    return "Ola, $nome! Bem-vindo ao sistema. 👋";
+}
+
+?>
