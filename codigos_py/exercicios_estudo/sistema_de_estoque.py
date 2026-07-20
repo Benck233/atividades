fc1b6d8bef0950d2694aefe54f030dde51297925
Benck233 <169964@upf.br>
2026-07-20 17:47:17 -0300

    sistema_de_estoque

--- codigos_py/exercicios_estudo/sistema_de_estoque.py
+++ codigos_py/exercicios_estudo/sistema_de_estoque.py
@@ -20,6 +20,20 @@ while True:
         #print(relatorio)
         break
 
+    if int(escolha) ==1:
+
+        print("\n selecine de 1 a 3 para adicionar, caso contrario aperta 4 para sair\n")
+
+        numero_if=1
+        for produto, info, in estoque.items():
+            print(f"{numero_if}. {produto}, possui {info["quantidade"]} no estoque, e o preço por unidade está em R$ {info["preco"]}.")
+            numero_if+=1
+
+        if int(escolha)==1:
+            estoque["Smartphone"]["quantidade"]
+
+
+
     if int(escolha) == 3:
         numero_if=1
         for produto, info, in estoque.items():
@@ -33,7 +47,9 @@ while True:
             print(f"{estoque["Smartphone"]}")
         elif int(escolha) ==2:
             print(f"A quantidade de Noebok no estoque é de {estoque['Notebook']["quantidade"]}")
-        
+        elif int(escolha) ==3:
+            print(f"A quantidade de fone bluethof no estoque é de {estoque['Fone Bluetooth']["quantidade"]}")
+
         if int(escolha) == 4:
 
             break
