3564a8d75b4a78da22719634b903e89f3a80261c
Benck233 <169964@upf.br>
2026-07-20 16:51:32 -0300

    sistema_de_estoque

---
+++ codigos_py/exercicios_estudo/sistema_de_estoque.py
@@ -0,0 +1,33 @@
+estoque = {
+    "Smartphone": {"quantidade": 10, "preco": 800},
+    "Notebook": {"quantidade": 5, "preco": 2500},
+    "Fone Bluetooth": {"quantidade": 20, "preco": 150}
+}
+
+opcoes={"adicionar":1,"remover":2,"ver estoque":3,"sair":4}
+
+numero=1
+
+for opcao, valor in opcoes.items():
+    print(f"{numero}. {opcao} = {valor}.")
+    numero+=1
+
+
+while True:
+    escolha=input()
+    
+    if int(escolha) == 4:
+        #print(relatorio)
+        break
+
+    if int(escolha) == 3:
+        numero_if=1
+        for produto, info, in estoque.items():
+            print(f"{numero_if}. {produto}, possui {info["quantidade"]} no estoque, e o preço por unidade está em R$ {info["preco"]}.")
+            numero_if+=1
+
+        escolha=input()
+
+        if int(escolha) == 1:
+        
+            print(f"{estoque["Smartphone"]}")
