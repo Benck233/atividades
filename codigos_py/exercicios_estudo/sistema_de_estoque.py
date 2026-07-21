4160b801a671337768ee01e073f9c52092670f4c
Benck233 <169964@upf.br>
2026-07-21 11:03:31 -0300

    dicas_do_fabricio

--- codigos_py/exercicios_estudo/sistema_de_estoque.py
+++ codigos_py/exercicios_estudo/sistema_de_estoque.py
@@ -21,8 +21,12 @@ while True:
 
     for opcao, valor in opcoes.items():
         print(f"{numero}. {opcao} = {valor}.")
-
         numero+=1
+
+        if numero>4:
+            numero=0
+            numero+=1
+        
 
     escolha=input()
     
@@ -109,20 +113,9 @@ while True:
         for produto, info, in estoque.items():
             print(f"{numero_if}. {produto}, possui {info["quantidade"]} no estoque, e o preço por unidade está em R$ {info["preco"]}.")
             numero_if+=1
+        continue
 
-        escolha=input()
-
-        if int(escolha) == 1:
-            
-            print(f"{estoque["Smartphone"]}")
-        elif int(escolha) ==2:
-            print(f"A quantidade de Noebok no estoque é de {estoque['Notebook']["quantidade"]}")
-        elif int(escolha) ==3:
-            print(f"A quantidade de fone bluethof no estoque é de {estoque['Fone Bluetooth']["quantidade"]}")
-
-        if int(escolha) == 4:
-
-            break
+        
 
 
 novo_estoque={}
