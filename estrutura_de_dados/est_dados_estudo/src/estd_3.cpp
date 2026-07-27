9b121c6bfd7b698b8b241b08b6ffd0d4f40bc191
Benck233 <169964@upf.br>
2026-07-27 17:33:00 -0300

    estd_cpp

--- estrutura_de_dados/est_dados_estudo/src/estd_3.cpp
+++ estrutura_de_dados/est_dados_estudo/src/estd_3.cpp
@@ -1,2 +1,29 @@
 #include <iostream>
 #include <string>
+using namespace std;
+
+
+int main()
+{
+    {
+    string nome;
+
+    cout << "Digite seu nome: ";
+    cin >> nome;
+
+    cout << "Ola " << nome << "!\n";
+    }
+
+    {int idade;
+
+    std::cout << "Digite sua idade: ";
+    std::cin >> idade;
+
+    std::cout << "Voce tem " << idade << " anos.\n";
+
+    }
+    return 0;
+
+}
+    
+
