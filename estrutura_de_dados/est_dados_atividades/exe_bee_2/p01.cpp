45bf4c9614cdb62d9ef174aa18080dded2f005a3
Benck233 <169964@upf.br>
2026-08-17 17:04:20 -0300

    3_exercicios_beecrowd

--- estrutura_de_dados/est_dados_atividades/exe_bee_2/p01.cpp
+++ estrutura_de_dados/est_dados_atividades/exe_bee_2/p01.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
+#include <iomanip>
+using namespace std;
+
 
 int main(){
 
- vetor[10];
- 
+double v[100];
+
+for (int i = 0; i < 100; i++)
+{
+    cin>>v[i];
+}
+
+for (int  i = 0; i < 100; i++)
+{
+    if (v[i]<=10){
 
 
-    return 0;
+        cout<<fixed<<setprecision(1);
+        cout<< "A[" << i << "] = " << v[i] << endl;
+
+    }
+}
+
+return 0;
 }
