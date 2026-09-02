9020dc11a7b2674c2ad11a920b9336737f684bef
Benck233 <169964@upf.br>
2026-09-02 12:17:57 -0300

    possivel_solução

--- estrutura_de_dados/atividades_beecrowd/p01.cpp
+++ estrutura_de_dados/atividades_beecrowd/p01.cpp
@@ -2,6 +2,34 @@
 #include <algorithm>
 using namespace std;
 
+
+
+//talvez usa assim
+/*int main(){
+    cout<<"digite uma entrada"<<endl;
+    const int MAX =1000;
+    int n;
+    cin>>n;
+    int numero_atletas= n;
+    int tempo_atletas[MAX];
+
+    if(n > MAX){
+        cout<<"Quantidade invalida";
+        retunr 1;
+    }
+    
+    for (int i = 0; i < n; i++)
+    {
+        cin>>tempo_atletas[i];
+    }
+    
+    
+    
+
+
+    return 0;
+}*/
+
 int main(){
     cout<<"digite uma entrada"<<endl;
     int n;
